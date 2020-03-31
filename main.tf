965e054bca0c82fdaab30e95c057898a921cbfa0
djangulo <djal@tuta.io>
2020-03-31 19:32:08 -0400

    bumped module version

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "docker_compose_host" {
   source  = "djangulo/docker-compose-host/digitalocean"
-  version = "0.2.4"
+  version = "0.2.5"
 
   do_token     = var.do_token
   droplet_name = "Projects"
